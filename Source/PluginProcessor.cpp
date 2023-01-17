eae7cb4a3601fcdd05fae3cb1508e4797dfdd45c
Kristel Cocoli <kristel.cocoli@studenti.unipd.it>
2023-01-17 21:47:04 +0100

    Processing element 2

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -311,40 +311,24 @@ juce::AudioProcessorValueTreeState::ParameterLayout ProvaDSPAudioProcessor::crea
     juce::AudioProcessorValueTreeState::ParameterLayout layout;
     
     
-    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"LowCut Freq", 1},
-                                                               "LowCut Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
-                                                               20.f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"LowCut Freq", 1}, "LowCut Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f), 20.f));
         
-    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"HighCut Freq", 1},
-                                                               "HighCut Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
-                                                               20000.f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"HighCut Freq", 1}, "HighCut Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f), 20000.f));
         
-    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Freq", 1},
-                                                               "Peak Freq",
-                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
-                                                               750.f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Freq", 1}, "Peak Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f), 750.f));
         
-    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Gain", 1},
-                                                               "Peak Gain",
-                                                               juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
-                                                               0.0f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Gain", 1}, "Peak Gain", juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f), 0.0f));
         
-    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Quality", 1},
-                                                               "Peak Quality",
-                                                               juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
-                                                               1.f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID {"Peak Quality", 1}, "Peak Quality", juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f), 1.f));
         
-    
     juce::StringArray stringArray;
-        for( int i = 0; i < 4; ++i )
-        {
-            juce::String str;
-            str << (12 + i*12);
-            str << " db/Oct";
-            stringArray.add(str);
-        }
+    for( int j = 0; j < 4; ++j )
+    {
+        juce::String value;
+        value << (12 + j*12);
+        value << " db/Oct";
+        stringArray.add(value);
+    }
         
     layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID {"LowCut Slope", 1}, "LowCut Slope", stringArray, 0));
     layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID {"HighCut Slope", 1}, "HighCut Slope", stringArray, 0));
@@ -357,8 +341,6 @@ juce::AudioProcessorValueTreeState::ParameterLayout ProvaDSPAudioProcessor::crea
         return layout;
     }
 
-    //==============================================================================
-    // This creates new instances of the plugin..
     juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
     {
         return new ProvaDSPAudioProcessor();
